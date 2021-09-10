53f7cb3259780cfca7c723d619a1f82b23fc370a
abhiamishra <abhiamishra0@gmail.com>
2021-09-10 15:03:07 -0500

    Initial functions and logo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # shakeR
 An analysis and visualization R package that works with publicly available soccer data
+
+# shakeR <img src="shakeR.png" align="right" width="181" height="201"/>
--- shakeR/R/plot_pass.R
+++ shakeR/R/plot_pass.R
@@ -5,13 +5,14 @@
 #' @param pass_data The dataframe that stores your passing data
 #' @param standard TRUE if only plotting successful/unsuccessful passes
 #' @param background Pick between white or dark background.
-#' @param prog indicates whether to have progressive passes
+#' @param prog indicates whether to map out progressive passes
 #' @example
 #' plot = plot_pass(passingdata, standard="st", background = "dark")
 
 
 #' @export
-plot_pass <- function(pass_data, standard="", prog = "", team="", player="", background=""){
+plot_pass <- function(pass_data, standard="", prog=FALSE, cross=FALSE, shot=FALSE, switch=FALSE, 
+                      to_zone = "" , from_zone= "", distance= "", outcome="all", team="", player="", background=""){
   if(team != "" && player != ""){
     pass_data = pass_data %>%
       filter(team.name == team) %>%
@@ -26,23 +27,54 @@ plot_pass <- function(pass_data, standard="", prog = "", team="", player="", bac
       filter(player.name == player)
   }
   
+  if(outcome == "suc"){
+    pass_data = pass_data %>%
+      filter(is.na(pass.outcome.name))
+  }
+  else if(outcome == "unsuc"){
+    pass_data = pass_data %>%
+      filter(!is.na(pass.outcome.name))
+  }
+  
   if(standard=="st"){
     pass_data$pass.outcome.name = replace_na(pass_data$pass.outcome.name, "Successful")
     pass_data = pass_data %>% mutate(colorOutcome = ifelse(pass.outcome.name == "Successful",
-                                                           "S",
-                                                         "US"))
+                                                           "Successful",
+                                                         "Unsuccessful"))
   }
-  if(prog == "prog"){
+  
+  
+  if(prog == TRUE){
     pass_data = pass_data %>%
       mutate(start= sqrt((100-location.x)^2 + (50-location.y)^2)) %>%
       mutate(end = sqrt((100-pass.end_location.x)^2 + (50-pass.end_location.y)^2)) %>%
       mutate(isProg = ifelse(end <= 0.75*start,
                              1,
-                             0)) %>%
-               filter(isProg == 1)
+                             0))
+    
+    pass_data = pass_data %>% filter(isProg == 1)
   }
   
+  if(cross == TRUE){
+    pass_data = pass_data %>%
+      filter(pass.cross == TRUE)
+  }
+  
+  if(shot == TRUE){
+    pass_data = pass_data %>%
+      filter(pass.shot_assist == TRUE)
+  }
+  
+  if(switch==TRUE){
+    pass_data = pass_data %>% mutate(delta_y = abs(
+      pass.end_location.y - location.y
+    )) %>%
+      filter(delta_y >= 35)
+  }
+  
+  
   if(nrow(pass_data) > 0){
+    
     colour_b = "black"
     fill_b = "white"
     if(background == "dark"){
@@ -57,14 +89,16 @@ plot_pass <- function(pass_data, standard="", prog = "", team="", player="", bac
       theme_pitch()+
       theme(panel.background = element_rect(fill = fill_b))
     
-    plot+
-      layer(geom=geom_segment(aes(x=location.x,y=location.y, 
-                                                  xend=pass.end_location.x, yend=(pass.end_location.y), color=colorOutcome),
-                              lineend = "round", size = 1.5, arrow=arrow(length=unit(0.10, "inches"))),stat = "identity",position=1)+
-      facet_grid(~colorOutcome) +
-      labs(
-        color = "Outcome of Pass"
-      )
+    if(standard=="st"){
+      plot = plot +
+        geom_segment(aes(x=location.x,y=location.y, 
+                                         xend=pass.end_location.x, yend=(pass.end_location.y), color=colorOutcome),
+                     lineend = "round", size = 1.5, arrow=arrow(length=unit(0.10, "inches")),stat="identity",position="identity")+
+        facet_grid(~colorOutcome) +
+        labs(
+          color = "Outcome of Pass"
+        )
+    }
   }
   
   plot

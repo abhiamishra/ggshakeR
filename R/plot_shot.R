80e12f8338e0c0bd4bf4719cc38a019000e75999
Ryo Nakagawara <ryonakagawara@gmail.com>
2022-05-02 14:14:12 +0900

    - change 'shotdata' argument to 'data' in `plot_shot()`
    references #27

--- R/plot_shot.R
+++ R/plot_shot.R
@@ -3,7 +3,7 @@
 #' This function allows for data, that has to be scraped from Understat, to be used
 #' for plotting shots.
 #'
-#' @param shotdata Dataframe that houses shot data. Dataframe must contain atleast the following columns: X,Y,xG,result,name
+#' @param data Dataframe that houses shot data. Dataframe must contain atleast the following columns: X,Y,xG,result,name
 #' @param type Type of showcasing the shotmap: hexbin, density, point (default)
 #' @param bin_size Bin size for creating bins. Use this when using hexbin shotmap. Default = 30.
 #' @param highlight_goals to choose to display only the goals in a different colour.
@@ -20,16 +20,16 @@
 #'
 #' @examples
 #' \dontrun{
-#' plot <- plot_shot(shotdata, type = "hexbin", bin_size = 20, avg_loc = TRUE, highlight_goals = FALSE)
+#' plot <- plot_shot(data, type = "hexbin", bin_size = 20, avg_loc = TRUE, highlight_goals = FALSE)
 #' }
 
-plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", avg_loc = "", theme = "") {
+plot_shot <- function(data, type = "", bin_size = 30, highlight_goals = "", avg_loc = "", theme = "") {
   
-  if ((nrow(shotdata) > 0) &&
-       sum(c("X", "Y", "xG", "result", "player") %in% names(shotdata)) == 5) {
+  if ((nrow(data) > 0) &&
+       sum(c("X", "Y", "xG", "result", "player") %in% names(data)) == 5) {
     
-    last <- sub(".* ", "", shotdata$player[nrow(shotdata)])
-    first <- sub(" .*", "", shotdata$player[nrow(shotdata)])
+    last <- sub(".* ", "", data$player[nrow(data)])
+    first <- sub(" .*", "", data$player[nrow(data)])
     player_name <- paste(first, last, "Shot Map", sep = "\n")
     
     fill_b <- ""
@@ -65,7 +65,7 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
     }
     
     ## base plot ----
-    plot <- shotdata %>%
+    plot <- data %>%
       ggplot() +
       annotate_pitch(dimensions = pitch_statsbomb, colour = colour_b,
                      fill = fill_b) +
@@ -73,14 +73,14 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
       theme(panel.background = element_rect(fill = fill_b))
     
     
-    if (nrow(shotdata) >= 1) {
-      shotdata <- shotdata %>%
+    if (nrow(data) >= 1) {
+      data <- data %>%
         mutate(X = 120 * X) %>%
         mutate(Y = 80 * Y)
       
-      total_xG <- sum(shotdata$xG)
-      total_goal <- sum(shotdata$result == "Goal")
-      xg_sot <- total_xG / nrow(shotdata)
+      total_xG <- sum(data$xG)
+      total_goal <- sum(data$result == "Goal")
+      xg_sot <- total_xG / nrow(data)
       
       if (avg_loc == TRUE || avg_loc == "") {
         
@@ -88,10 +88,10 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
           
           if (highlight_goals == FALSE || highlight_goals == "") { ## Don't highlight goals ----
             plot  <- plot +
-              geom_point(data = shotdata, aes(x = X, y = (80 - Y), size = xG, color = result), alpha = 0.7) +
+              geom_point(data = data, aes(x = X, y = (80 - Y), size = xG, color = result), alpha = 0.7) +
               scale_size_continuous(range = c(0.5, 7)) +
-              geom_vline(xintercept = mean(shotdata$X), color = colorLine, linetype = 2, size = 1.5) +
-              geom_hline(yintercept = mean(shotdata$Y), color = colorLine, linetype = 2, size = 1.5) +
+              geom_vline(xintercept = mean(data$X), color = colorLine, linetype = 2, size = 1.5) +
+              geom_hline(yintercept = mean(data$Y), color = colorLine, linetype = 2, size = 1.5) +
               geom_point(x = 86, y = 10, size = 40, color = colorText, shape = 1) +
               geom_text(x = 86, y = 10, label = format(round(total_xG, 2)), color = colorText, size = 10) +
               geom_text(x = 80, y = 10, label = "xG", color = colorText, size = 10) +
@@ -109,15 +109,15 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
               )
           } else if (highlight_goals == TRUE) { ## Highlight goals ----
             
-            shotdata <- shotdata %>%
+            data <- data %>%
               mutate(isGoal = ifelse(result == "Goal", "Goal", "No Goal"))
             
             plot <- plot +
-              geom_point(data = shotdata, aes(x = X, y = (80 - Y), size = xG, fill = isGoal), color = colour_b, pch = 21, alpha = 0.7) +
+              geom_point(data = data, aes(x = X, y = (80 - Y), size = xG, fill = isGoal), color = colour_b, pch = 21, alpha = 0.7) +
               scale_fill_manual(values = c("#2fc22f", fill_b)) +
               scale_size_continuous(range = c(0.5, 7)) +
-              geom_vline(xintercept = mean(shotdata$X), color = colorLine, linetype = 2, size = 1.5) +
-              geom_hline(yintercept = mean(shotdata$Y), color = colorLine, linetype = 2, size = 1.5) +
+              geom_vline(xintercept = mean(data$X), color = colorLine, linetype = 2, size = 1.5) +
+              geom_hline(yintercept = mean(data$Y), color = colorLine, linetype = 2, size = 1.5) +
               geom_point(x = 86, y = 10, size = 40, color = colorText, shape = 1) +
               geom_text(x = 86, y = 10, label = format(round(total_xG, 2)), color = colorText, size = 10) +
               geom_text(x = 80, y = 10, label = "xG", color = colorText, size = 10) +
@@ -136,12 +136,12 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
           } 
         } else if (type == "density") { ## DENSITY ----
           plot <- plot +
-            stat_density_2d(data = shotdata, aes(x = X, y = (80 - Y), fill = ..level..), geom = "polygon",
+            stat_density_2d(data = data, aes(x = X, y = (80 - Y), fill = ..level..), geom = "polygon",
                             alpha = 0.7) +
             scale_fill_gradient(high = "#6BFF84", low = "#01141D") +
             scale_size_continuous(range = c(0.5, 7)) +
-            geom_vline(xintercept = mean(shotdata$X), color = colorLine, linetype = 2, size = 1.5) +
-            geom_hline(yintercept = mean(shotdata$Y), color = colorLine, linetype = 2, size = 1.5) +
+            geom_vline(xintercept = mean(data$X), color = colorLine, linetype = 2, size = 1.5) +
+            geom_hline(yintercept = mean(data$Y), color = colorLine, linetype = 2, size = 1.5) +
             geom_point(x = 86, y = 10, size = 40, color = colorText, shape = 1) +
             geom_text(x = 86, y = 10, label = format(round(total_xG, 2)), color = colorText, size = 10) +
             geom_text(x = 80, y = 10, label = "xG", color = colorText, size = 10) +
@@ -157,11 +157,11 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
           
         } else if (type == "hexbin") { ## HEXBIN ----
           plot <- plot +
-            geom_hex(data = shotdata, aes(x = X, y = (80 - Y)), bins = bin_size) +
+            geom_hex(data = data, aes(x = X, y = (80 - Y)), bins = bin_size) +
             scale_fill_continuous(type = "viridis") +
             scale_size_continuous(range = c(0.5, 7)) +
-            geom_vline(xintercept = mean(shotdata$X), color = colorLine, linetype = 2, size = 1.5) +
-            geom_hline(yintercept = mean(shotdata$Y), color = colorLine, linetype = 2, size = 1.5) +
+            geom_vline(xintercept = mean(data$X), color = colorLine, linetype = 2, size = 1.5) +
+            geom_hline(yintercept = mean(data$Y), color = colorLine, linetype = 2, size = 1.5) +
             geom_point(x = 86, y = 10, size = 40, color = colorText, shape = 1) +
             geom_text(x = 86, y = 10, label = format(round(total_xG, 2)), color = colorText, size = 10) +
             geom_text(x = 80, y = 10, label = "xG", color = colorText, size = 10) +
@@ -183,7 +183,7 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
           
           if (highlight_goals == FALSE || highlight_goals == "") {
             plot <- plot +
-              geom_point(data = shotdata, aes(x = X, y = (80 - Y), size = xG, color = result), alpha = 0.7) +
+              geom_point(data = data, aes(x = X, y = (80 - Y), size = xG, color = result), alpha = 0.7) +
               scale_size_continuous(range = c(0.5, 7)) +
               geom_point(x = 86, y = 10, size = 40, color = colorText, shape = 1) +
               geom_text(x = 86, y = 10, label = format(round(total_xG, 2)), color = colorText, size = 10) +
@@ -202,11 +202,11 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
               )
           } else if (highlight_goals == TRUE) {
             
-            shotdata <- shotdata %>%
+            data <- data %>%
               mutate(isGoal = ifelse(result == "Goal", "Goal", "No Goal"))
             
             plot <- plot +
-              geom_point(data = shotdata, aes(x = X, y = (80 - Y), size = xG, fill = isGoal), color = colour_b, pch = 21, alpha = 0.7) +
+              geom_point(data = data, aes(x = X, y = (80 - Y), size = xG, fill = isGoal), color = colour_b, pch = 21, alpha = 0.7) +
               scale_fill_manual(values = c("#2fc22f", fill_b)) +
               scale_size_continuous(range = c(0.5, 7)) +
               geom_point(x = 86, y = 10, size = 40, color = colorText, shape = 1) +
@@ -227,7 +227,7 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
           } 
         } else if (type == "density") {
           plot <- plot +
-            stat_density_2d(data = shotdata, aes(x = X, y = (80 - Y), fill = ..level..), geom = "polygon",
+            stat_density_2d(data = data, aes(x = X, y = (80 - Y), fill = ..level..), geom = "polygon",
                             alpha = 0.7) +
             scale_fill_gradient(high = "#6BFF84", low = "#01141D") +
             scale_size_continuous(range = c(0.5, 7)) +
@@ -246,7 +246,7 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
           
         } else if (type == "hexbin") {
           plot <- plot +
-            geom_hex(data = shotdata, aes(x = X, y = (80 - Y)), bins = bin_size) +
+            geom_hex(data = data, aes(x = X, y = (80 - Y)), bins = bin_size) +
             scale_fill_continuous(type = "viridis") +
             scale_size_continuous(range = c(0.5, 7)) +
             geom_point(x = 86, y = 10, size = 40, color = colorText, shape = 1) +
@@ -271,6 +271,7 @@ plot_shot <- function(shotdata, type = "", bin_size = 30, highlight_goals = "", 
       
       plot
     } else{
+      ## add WARNING that there were no rows in the data frame passed into function??
       plot
     }
   }

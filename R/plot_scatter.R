d5fa8d238e28a32015694150df781e1db9703e24
Ryo Nakagawara <ryonakagawara@gmail.com>
2022-05-08 19:08:44 +0900

    - fix undeclared 'plot' object if there's no data or columns specified. - early error for when no data is passed into function
    references #27

--- R/plot_scatter.R
+++ R/plot_scatter.R
@@ -46,129 +46,133 @@ plot_scatter <- function(data, scatter_x = "", scatter_y = "", scatter_label = "
                          caption = "",
                          caption_size = 10) {
   if (nrow(data) > 0) {
-    
-    ## Pre-processing ----
-    total <- 0
-    
-    if (scatter_x %in% names(data) && scatter_y %in% names(data)) {
-      selection <- c(scatter_x, scatter_y)
-      
-      renaming <- c('scatter_x', 'scatter_y')
-      
-      total <- 2
-      
-      col_var <- ""
-      size_var <- ""
-      
-      if (scatter_label != "" && scatter_label %in% names(data)) {
-        if (scatter_label %in% selection == TRUE) {
-          data[, "scatter_label"] <- data[, scatter_label]
-          
-          selection <- append(selection, 'scatter_label')
-          renaming <- append(renaming, 'scatter_label')
-        } else{
-          selection <- append(selection, scatter_label)
-          renaming <- append(renaming, 'scatter_label')
-        }
-        total <- total + 1
-      }
-      
-      if (set_size_var != "" && set_size_var %in% names(data)) {
-        if (set_size_var %in% selection == TRUE) {
-          data[, "set_size_var"] <- data[, set_size_var]
-          
-          selection <- append(selection, 'set_size_var')
-          renaming <- append(renaming, 'set_size_var')
-        } else {
-          selection <- append(selection, set_size_var)
-          renaming <- append(renaming, 'set_size_var')
-        }
-        total <- total + 1
-        
-        size_var <- set_size_var
-      }
-      
-      if (set_color_var != "" && set_color_var %in% names(data)) {
-        if (set_color_var %in% selection == TRUE) {
-          data[, "set_color_var"] <- data[, set_color_var]
-          
-          selection <- append(selection, 'set_color_var')
-          renaming <- append(renaming, 'set_color_var')
-        } else{
-          selection <- append(selection, set_color_var)
-          renaming <- append(renaming, 'set_color_var')
-        }
-        total <- total + 1
-        
-        col_var <- set_color_var
-      }
-      
-      data <- data %>%
-        select(c(selection))
-      
-      
-      for (i in 1:total) {
-        names(data)[i] <- renaming[i]
-      }
-      #Preprocessing over
-      
-      # Plotting ----
-      plot <- data %>%
-        ggplot(aes(x = scatter_x, y = scatter_y))
-      
-      x_title <- scatter_x
-      y_title <- scatter_y
-      
-      if (set_size_var != "" && set_color_var != "") {
-        plot <- plot +
-          geom_point(aes(size = set_size_var, color = set_color_var))
-      } else if (set_size_var == "" && set_color_var != "") {
-        plot <- plot +
-          geom_point(aes(color = set_color_var), size = set_size_num)
-      } else if (set_size_var != "" && set_color_var == "") {
-        plot <- plot +
-          geom_point(aes(size = set_size_var), color = set_color_num)
-      } else if (set_size_var == "" && set_color_var == "") {
-        plot <- plot +
-          geom_point(size = set_size_num, color = set_color_num)
-      }
-      
-      if (scatter_label != "") {
-        plot <- plot +
-          geom_label_repel(aes(x = scatter_x, y = scatter_y, label = scatter_label), max.overlaps = 2)
-      }
-      
-      ## Theme ----
-      if (theme == "classic") {
-        plot <- plot +
-          theme_classic()
-      } else if (theme == "minimal") {
-        plot <- plot +
-          theme_minimal()
-      } else if (theme == "grey") {
-        plot <- plot +
-          theme_grey()
-      } else if (theme == "bw") {
-        plot <- plot +
-          theme_bw()
-      }
-      
-      plot <- plot +
-        labs(
-          title = title,
-          subtitle = subtitle,
-          caption = caption,
-          color = col_var,
-          size = size_var,
-          x = x_title,
-          y = y_title
-        ) +
-        theme(
-          plot.title = element_text(color = "black", size = title_size),
-          plot.subtitle = element_text(color = "black", size = subtitle_size),
-          plot.caption = element_text(color = "black", size = caption_size)
-        )
-    }
+    stop("Please input a data.frame into the 'data' argument.")
   }
-  plot
+  
+  ## Pre-processing ----
+  total <- 0
+  
+  if (scatter_x %in% names(data) && scatter_y %in% names(data)) {
+    selection <- c(scatter_x, scatter_y)
+    
+    renaming <- c('scatter_x', 'scatter_y')
+    
+    total <- 2
+    
+    col_var <- ""
+    size_var <- ""
+    
+    if (scatter_label != "" && scatter_label %in% names(data)) {
+      if (scatter_label %in% selection == TRUE) {
+        data[, "scatter_label"] <- data[, scatter_label]
+        
+        selection <- append(selection, 'scatter_label')
+        renaming <- append(renaming, 'scatter_label')
+      } else{
+        selection <- append(selection, scatter_label)
+        renaming <- append(renaming, 'scatter_label')
+      }
+      total <- total + 1
+    }
+    
+    if (set_size_var != "" && set_size_var %in% names(data)) {
+      if (set_size_var %in% selection == TRUE) {
+        data[, "set_size_var"] <- data[, set_size_var]
+        
+        selection <- append(selection, 'set_size_var')
+        renaming <- append(renaming, 'set_size_var')
+      } else {
+        selection <- append(selection, set_size_var)
+        renaming <- append(renaming, 'set_size_var')
+      }
+      total <- total + 1
+      
+      size_var <- set_size_var
+    }
+    
+    if (set_color_var != "" && set_color_var %in% names(data)) {
+      if (set_color_var %in% selection == TRUE) {
+        data[, "set_color_var"] <- data[, set_color_var]
+        
+        selection <- append(selection, 'set_color_var')
+        renaming <- append(renaming, 'set_color_var')
+      } else{
+        selection <- append(selection, set_color_var)
+        renaming <- append(renaming, 'set_color_var')
+      }
+      total <- total + 1
+      
+      col_var <- set_color_var
+    }
+    
+    data <- data %>%
+      select(c(selection))
+    
+    
+    for (i in 1:total) {
+      names(data)[i] <- renaming[i]
+    }
+    #Preprocessing over
+    
+    # Plotting ----
+    plot <- data %>%
+      ggplot(aes(x = scatter_x, y = scatter_y))
+    
+    x_title <- scatter_x
+    y_title <- scatter_y
+    
+    if (set_size_var != "" && set_color_var != "") {
+      plot <- plot +
+        geom_point(aes(size = set_size_var, color = set_color_var))
+    } else if (set_size_var == "" && set_color_var != "") {
+      plot <- plot +
+        geom_point(aes(color = set_color_var), size = set_size_num)
+    } else if (set_size_var != "" && set_color_var == "") {
+      plot <- plot +
+        geom_point(aes(size = set_size_var), color = set_color_num)
+    } else if (set_size_var == "" && set_color_var == "") {
+      plot <- plot +
+        geom_point(size = set_size_num, color = set_color_num)
+    }
+    
+    if (scatter_label != "") {
+      plot <- plot +
+        geom_label_repel(aes(x = scatter_x, y = scatter_y, label = scatter_label), max.overlaps = 2)
+    }
+    
+    ## Theme ----
+    if (theme == "classic") {
+      plot <- plot +
+        theme_classic()
+    } else if (theme == "minimal") {
+      plot <- plot +
+        theme_minimal()
+    } else if (theme == "grey") {
+      plot <- plot +
+        theme_grey()
+    } else if (theme == "bw") {
+      plot <- plot +
+        theme_bw()
+    }
+    
+    plot <- plot +
+      labs(
+        title = title,
+        subtitle = subtitle,
+        caption = caption,
+        color = col_var,
+        size = size_var,
+        x = x_title,
+        y = y_title
+      ) +
+      theme(
+        plot.title = element_text(color = "black", size = title_size),
+        plot.subtitle = element_text(color = "black", size = subtitle_size),
+        plot.caption = element_text(color = "black", size = caption_size)
+      )
+  } else {
+    stop("Please input columns for the 'scatter_x' and/or 'scatter_y' arguments.")
+  }
+  
+  return(plot)
 }

1fec3b93e740827c359d9d28514c6c79b842c58c
Harsh Krishna <harsh.krishna2016@gmail.com>
2022-05-10 11:37:09 +0530

    new fixes to plot_convexhull

--- R/utils.R
+++ R/utils.R
@@ -12,3 +12,20 @@ text_wrap <- function(x) {
   
   return(final_text) 
 }
+
+#' @title hull_fun
+#' @description Create boundaries for convex hull
+#' @param x data frame
+#' @return data frame
+#' @rdname hull_fun
+#' @keywords internal 
+#' @importFrom dplyr filter slice
+  
+hull_fun <- function(data) {
+  hull_data <- data %>%  
+    filter((x > x_low) & (x < x_high)) %>%
+    filter((y > y_low) & (y < y_high)) %>%
+    slice(chull(x, y))
+
+  return(hull_data)
+}

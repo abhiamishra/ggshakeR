6ba0dacbb71021dc973a3d8d6a25d1799ccb5dc2
abhiamishra <abhiamishra0@gmail.com>
2022-05-12 10:26:39 -0500

    Removing EPV Grid - voronoi-#28

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,7 +2,7 @@
 xTGrid <- read.csv("./inst/extdata/xT.csv")
 usethis::use_data(xTGrid, overwrite = TRUE, compress="xz")
 
-EPVGrid <- read.csv("./inst/extdata/EPV.csv")
-usethis::use_data(EPVGrid, overwrite = TRUE, compress="xz")
+#EPVGrid <- read.csv("./inst/extdata/EPV.csv")
+#usethis::use_data(EPVGrid, overwrite = TRUE, compress="xz")
 
 

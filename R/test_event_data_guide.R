bf3fa8a8d7df6952ca9a08eae9a25c43e73a28ee
Harsh Krishna <harsh.krishna2016@gmail.com>
2022-05-28 14:10:00 +0530

    add sample event dataset to package

---
+++ R/test_event_data_guide.R
@@ -0,0 +1,8 @@
+#' @title Sample event data frame
+#'
+#' @description Event \code{dataframe} to play with and understand usage of functions
+#'
+#' @format A \code{dataframe} with the columns `x`, `y`, `finalX`, `finalY`, `minute`,
+#'  `playerId`, `teamId`, `outcome` and `type`.
+#'
+"TestEventData"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,9 @@
 ## code to prepare `DATASET` dataset goes here
 xTGrid <- read.csv("./inst/extdata/xT.csv")
-usethis::use_data(xTGrid, overwrite = TRUE, compress="xz")
+usethis::use_data(xTGrid, overwrite = TRUE, compress = "xz")
 
 EPVGrid <- read.csv("./inst/extdata/EPV.csv")
-usethis::use_data(EPVGrid, overwrite = TRUE, compress="xz")
+usethis::use_data(EPVGrid, overwrite = TRUE, compress = "xz")
 
-
+TestEventData <- read.csv("./inst/extdata/TestEventData.csv")
+usethis::use_data(TestEventData, overwrite = TRUE, compress = "xz")

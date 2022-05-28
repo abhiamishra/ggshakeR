a8a9c31769592a7565b64bce91c813dbd8d392d4
abhiamishra <abhiamishra0@gmail.com>
2022-05-28 11:30:40 -0500

    issue-#45-guide

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,8 @@
 ## code to prepare `DATASET` dataset goes here
 xTGrid <- read.csv("./inst/extdata/xT.csv")
-usethis::use_data(xTGrid, overwrite = TRUE, compress="xz")
+usethis::use_data(xTGrid, overwrite = TRUE, compress="xz", header=FALSE)
 
-EPVGrid <- read.csv("./inst/extdata/EPV.csv")
+EPVGrid <- read.csv("./inst/extdata/EPV.csv", header=FALSE)
 usethis::use_data(EPVGrid, overwrite = TRUE, compress="xz")
 
 

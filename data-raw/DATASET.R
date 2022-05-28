c582b2f8d8317ee2ca8d820aede230c6d792c2b3
Harsh Krishna <87293901+harshkrishna17@users.noreply.github.com>
2022-05-28 21:21:54 +0530

    add sample event dataset to package #59 (#60)
    * add sample event dataset to package
    
    * changing dataset name from test to sample
    
    closes #59

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,8 +1,9 @@
 ## code to prepare `DATASET` dataset goes here
 xTGrid <- read.csv("./inst/extdata/xT.csv")
-usethis::use_data(xTGrid, overwrite = TRUE, compress="xz")
+usethis::use_data(xTGrid, overwrite = TRUE, compress = "xz")
 
 EPVGrid <- read.csv("./inst/extdata/EPV.csv")
-usethis::use_data(EPVGrid, overwrite = TRUE, compress="xz")
+usethis::use_data(EPVGrid, overwrite = TRUE, compress = "xz")
 
-
+SampleEventData <- read.csv("./inst/extdata/SampleEventData.csv")
+usethis::use_data(SampleEventData, overwrite = TRUE, compress = "xz")
